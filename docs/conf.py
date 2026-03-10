3fbd25b90d504dd88b7fd203a57299c3e105bb8f
Rodda John <johnrod.john@gmail.com>
2026-03-10 13:04:13 -0400

    docs updating

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,22 @@ project = "cave"
 author = "Rodda John"
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
 ]
 html_theme = "furo"
+html_title = "cave"
+exclude_patterns = ["_generated"]
+add_module_names = False
+nitpicky = True
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "pydantic": ("https://docs.pydantic.dev/latest", None),
+    "sqlalchemy": ("https://docs.sqlalchemy.org/en/20", None),
+    "typer": ("https://typer.tiangolo.com", None),
+}
